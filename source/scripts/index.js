6a63f616f1021ac7af36d278f504b6727bb8921a
AreOks <aoksana@yandex.ru>
2025-02-11 23:57:42 +0300

    Верстает секцию stats

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,32 +1,42 @@
 /* в этот файл добавляет скрипты*/
 
-// Меню сайта
-const navMain = document.querySelector('.navigation');
-const navToggle = document.querySelector('.navigation__toggle');
+document.addEventListener('DOMContentLoaded', () => {
 
-navMain.classList.remove('navigation--nojs');
+  const navMain = document.querySelector('.navigation');
+  const navToggle = document.querySelector('.navigation__toggle');
 
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('navigation--closed')) {
-    navMain.classList.remove('navigation--closed');
-    navMain.classList.add('navigation--opened');
-  } else {
-    navMain.classList.add('navigation--closed');
-    navMain.classList.remove('navigation--opened');
+  // const slider = document.querySelector('.slider');
+
+  const map = document.querySelector('.map__location-static');
+
+
+  // Меню сайта
+  if (navMain) {
+    navMain.classList.remove('navigation--nojs');
   }
+
+  // Карта
+  if (map) {
+    map.classList.remove('map__location-static--nojs');
+  }
+
+  // Ползунок
+  // range = document.querySelector('.slider__range-js');
+  // range.addEventListener('input', () => {
+  //   slider.style.setProperty('--value', `${range.value }%`);
+  // }
+
+  navToggle.addEventListener('click', () => {
+
+    if (navMain.classList.contains('navigation--closed')) {
+      navMain.classList.remove('navigation--closed');
+      navMain.classList.add('navigation--opened');
+      navToggle.setAttribute('aria-label', 'закрыть меню');
+    } else {
+      navMain.classList.add('navigation--closed');
+      navMain.classList.remove('navigation--opened');
+      navToggle.setAttribute('aria-label', 'открыть меню');
+    }
+  }
+  );
 });
-
-// Ползунок
-// const slider = document.querySelector('.slider');
-// const range = document.querySelector('.slider__range-js');
-// range.addEventListener('input', () => {
-//   slider.style.setProperty('--value', `${range.value }%`);
-// });
-
-
-// Карта
-// const map = document.querySelector('.map');
-// const mapImage = document.querySelector('.map__image');
-
-// map.classList.remove('map--nojs');
-// mapImage.classList.remove('map__image--nojs');
