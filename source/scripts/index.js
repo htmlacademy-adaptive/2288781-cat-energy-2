93cabae7ac7726c36f72f98c254f023a45b3e960
AreOks <aoksana@yandex.ru>
2025-02-23 21:40:24 +0300

    Разбирается со сборкой, опять какая-то проблема

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,26 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const navMain = document.querySelector('.navigation');
   const navToggle = document.querySelector('.navigation__toggle');
 
-  // const slider = document.querySelector('.slider');
-
-  const map = document.querySelector('.map__location-static');
-
+  const slider = document.querySelector(".slider .slider input");
+  const img = document.querySelector(".slider__image .slider__image--after");
+  const dragLine = document.querySelector(".slider .slider__button");
 
   // Меню сайта
   if (navMain) {
     navMain.classList.remove('navigation--nojs');
   }
-
-  // Карта
-  if (map) {
-    map.classList.remove('map__location-static--nojs');
-  }
-
-  // Ползунок
-  // range = document.querySelector('.slider__range-js');
-  // range.addEventListener('input', () => {
-  //   slider.style.setProperty('--value', `${range.value }%`);
-  // }
 
   navToggle.addEventListener('click', () => {
 
@@ -39,4 +27,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
   );
-});
+
+
+  // Ползунок
+  slider.oninput = ()=>{
+    let sliderVal = slider.value;
+    dragLine.style.left = sliderVal + "%";
+    img.style.width = sliderVal + "%";
+  }
+
+
+// Карта
+// if (map) {
+//   map.classList.remove('map__location-static--nojs');
+// }
+
+// Пин
+// objectManager.objects.options.set({
+//   iconLayout: 'default#image',
+//   iconImageHref: '../images/maps',
+//   iconImageSize: [95, 37], // размеры картинки
+//   iconImageOffset: [-11, -30] // смещение картинки
+// });
