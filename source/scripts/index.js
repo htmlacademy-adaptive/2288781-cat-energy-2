4d8d6075d33577b2efcc0819d4d83a64efef7d8b
777aoksana <aoksana@yandex.ru>
2025-03-01 18:06:00 +0300

    fix: разбирается со слайдером

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,25 @@
 /* в этот файл добавляет скрипты*/
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const navMain = document.querySelector('.navigation');
+  const navToggle = document.querySelector('.navigation__toggle');
+
+  // Меню сайта
+  if (navMain) {
+    navMain.classList.remove('navigation--nojs');
+  }
+
+  navToggle.addEventListener('click', () => {
+
+    if (navMain.classList.contains('navigation--closed')) {
+      navMain.classList.remove('navigation--closed');
+      navMain.classList.add('navigation--opened');
+      navToggle.setAttribute('aria-label', 'закрыть меню');
+    } else {
+      navMain.classList.add('navigation--closed');
+      navMain.classList.remove('navigation--opened');
+      navToggle.setAttribute('aria-label', 'открыть меню');
+    }
+  });
+});
