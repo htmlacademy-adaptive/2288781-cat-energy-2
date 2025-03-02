c2a12b981aa658121fb66dfafe486e079ade3744
777aoksana <aoksana@yandex.ru>
2025-03-03 00:03:25 +0300

    fix: разбирается с секцией map

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,10 +2,10 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  // Меню сайта
   const navMain = document.querySelector('.navigation');
   const navToggle = document.querySelector('.navigation__toggle');
 
-  // Меню сайта
   if (navMain) {
     navMain.classList.remove('navigation--nojs');
   }
@@ -22,4 +22,92 @@ document.addEventListener('DOMContentLoaded', () => {
       navToggle.setAttribute('aria-label', 'открыть меню');
     }
   });
+
+  // Ползунок
+  const slider = document.querySelector('.slider');
+  const before = slider.querySelector('.slider__wrapper-before');
+  const changeBtn = document.querySelector('.slider__thumb');
+
+  if (slider) {
+
+    let isActive = false;
+    const width = slider.offsetWidth / 2;
+    before.style.width = `${width}px`;
+
+
+    const beforeAfterSlider = (cursorPosition) => {
+      const shift = Math.max(0, Math.min(cursorPosition, slider.offsetWidth));
+      before.style.width = `${shift}px`;
+      changeBtn.style.left = `${shift}px`;
+    };
+
+    const pauseEvents = (evt) => {
+      if (evt.stopPropagation) {
+        evt.stopPropagation();
+      }
+      if (evt.preventDefault) {
+        evt.preventDefault();
+      }
+      evt.cancelBubble = true;
+      evt.returnValue = false;
+      return false;
+    };
+
+    slider.addEventListener('mousedown', () => {
+      isActive = true;
+    });
+
+    slider.addEventListener('mouseup', () => {
+      isActive = false;
+    });
+
+    slider.addEventListener('mouseleave', () => {
+      isActive = false;
+    });
+
+
+    slider.addEventListener('mousemove', (evt) => {
+      if (!isActive) {
+        return;
+      }
+
+      let cursorPosition = evt.pageX;
+      cursorPosition -= slider.getBoundingClientRect().left;
+      beforeAfterSlider(cursorPosition);
+      pauseEvents(evt);
+    });
+
+    slider.addEventListener('touchstart', (evt) => {
+      evt.preventDefault();
+      isActive = true;
+    });
+
+    slider.addEventListener('touchend', (evt) => {
+      evt.preventDefault();
+      isActive = false;
+    });
+
+    slider.addEventListener('touchcancel', (evt) => {
+      evt.preventDefault();
+      isActive = false;
+    });
+
+
+    slider.addEventListener('touchmove', (evt) => {
+      evt.preventDefault();
+      if (!isActive) {
+        return;
+      }
+
+      let touchPosition;
+      let i;
+
+      for (i = 0; i < evt.changedTouches.length; i++) {
+        touchPosition = evt.changedTouches[i].pageX;
+        touchPosition -= slider.getBoundingClientRect().left;
+        beforeAfterSlider(touchPosition);
+        pauseEvents(evt);
+      }
+    });
+  }
 });
