7afdcbd57b012664f8774ff2c4ef5eb5d9330d47
777aoksana <aoksana@yandex.ru>
2025-04-21 08:05:23 +0300

    fix: разбирается с картой

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,30 +2,38 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Меню сайта + карта
-  const navNojs = document.querySelector('.main-header__navigation');
+  // Меню сайта
   const navMain = document.querySelector('.main-header__navigation');
+  const navNojs = document.querySelector('.main-header__navigation');
   const navToggle = document.querySelector('.main-header__toggle');
-  const LocationNojs = document.querySelector('.map__location');
+  const toggleNojs = document.querySelector('.main-header__toggle');
+
+  // Карта
+  const locationNojs = document.querySelector('.map__location-iframe');
 
   if (navNojs) {
     navNojs.classList.remove('main-header__navigation--nojs');
   }
 
-  if (LocationNojs) {
-    navNojs.classList.remove('map__location--nojs');
+  if (toggleNojs) {
+    toggleNojs.classList.remove('main-header__toggle--nojs');
+  }
+
+  if (locationNojs) {
+    locationNojs.classList.remove('map__location-iframe--nojs');
   }
 
   navToggle.addEventListener('click', () => {
-
     if (navMain.classList.contains('main-header__navigation--closed')) {
       navMain.classList.remove('main-header__navigation--closed');
       navMain.classList.add('main-header__navigation--opened');
-      navToggle.setAttribute('aria-label', 'закрыть меню');
+      navToggle.classList.add('main-header__toggle--opened');
+      navToggle.setAttribute('aria-label', 'открыть меню');
     } else {
       navMain.classList.add('main-header__navigation--closed');
       navMain.classList.remove('main-header__navigation--opened');
-      navToggle.setAttribute('aria-label', 'открыть меню');
+      navToggle.classList.add('main-header__toggle--closed');
+      navToggle.setAttribute('aria-label', 'закрыть меню');
     }
   });
 
@@ -115,3 +123,60 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+
+/// Как вставить пин(кастомный знак)
+// ymaps.ready(function () {
+//   var myMap = new ymaps.Map('map', {
+//           center: [55.751574, 37.573856],
+//           zoom: 9
+//       }, {
+//           searchControlProvider: 'yandex#search'
+//       }),
+
+//       // Создаём макет содержимого.
+//       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+//           '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+//       ),
+
+//       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+//           hintContent: 'Собственный значок метки',
+//           balloonContent: 'Это красивая метка'
+//       }, {
+//           // Опции.
+//           // Необходимо указать данный тип макета.
+//           iconLayout: 'default#image',
+//           // Своё изображение иконки метки.
+//           iconImageHref: 'images/myIcon.gif',
+//           // Размеры метки.
+//           iconImageSize: [30, 42],
+//           // Смещение левого верхнего угла иконки относительно
+//           // её "ножки" (точки привязки).
+//           iconImageOffset: [-5, -38]
+//       }),
+
+//       myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+//           hintContent: 'Собственный значок метки с контентом',
+//           balloonContent: 'А эта — новогодняя',
+//           iconContent: '12'
+//       }, {
+//           // Опции.
+//           // Необходимо указать данный тип макета.
+//           iconLayout: 'default#imageWithContent',
+//           // Своё изображение иконки метки.
+//           iconImageHref: 'images/ball.png',
+//           // Размеры метки.
+//           iconImageSize: [48, 48],
+//           // Смещение левого верхнего угла иконки относительно
+//           // её "ножки" (точки привязки).
+//           iconImageOffset: [-24, -24],
+//           // Смещение слоя с содержимым относительно слоя с картинкой.
+//           iconContentOffset: [15, 15],
+//           // Макет содержимого.
+//           iconContentLayout: MyIconContentLayout
+//       });
+
+//   myMap.geoObjects
+//       .add(myPlacemark)
+//       .add(myPlacemarkWithContent);
+// });
